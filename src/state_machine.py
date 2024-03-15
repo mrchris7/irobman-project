+ use moveitcommander to get position of end effector + fix the tiny rotation of raw (x_rot), pitch(y_rot) and yaw(z_rot)

src/state_machine.py
@@ -13,6 +13,7 @@ from nav_msgs.msg import Odometry
 from std_srvs.srv import SetBool
 from irobman_project.srv import CartesianMotionPlanning, JointMotionPlanning, SetPoints, GetPoints, GetPoses, PickCube, PlaceCube
 from pick_and_place_module.plan_scene import PlanScene
+from pick_and_place_module.eef_control import MoveGroupControl
 from typing import List
 SIMULATION = 0
 
@@ -45,7 +46,8 @@ class StateMachine:
         self.place_cube_client = rospy.ServiceProxy('/motion_planner/PlaceCube', PlaceCube)
         
         # TODO: create the corresponsing rospy.Publisher inside the motion planning node and publish the eff-pose (alternatively, create a service)
-        self.eef_pose_sub = rospy.Subscriber('/joint_states', JointState, self.handle_eff_pose)
+        self.move_group = MoveGroupControl().move_group
+        self.eef_pose = self.move_group.get_current_pose().pose
         
         # cube detection
         self.detection_client = rospy.ServiceProxy('CubeDetection', GetPoints)
@@ -159,9 +161,8 @@ class StateMachine:
 
         # method 1: neaest to end effector
         
-        # pst_eff = ros_numpy.numpify(self.pose_eef)[0:4, 3]
-        pst_eef = self.get_transformation("panda_hand", "world")
-        #pst_eef = self.pose_eef
+        pst_eef = ros_numpy.numpify(self.eef_pose)[0:4, 3]
+        # pst_eef = self.get_transformation("panda_hand", "world")
         # compute mininum distance and corresponding index
         dist = np.linalg.norm(psts_obj - pst_eef, axis=1)
         ind_min = np.argmin(dist)
@@ -169,7 +170,7 @@ class StateMachine:
         rospy.logwarn(f"choose pose with min distance: {best_pose}")
     
         # # method 2: least neighbours
-        # threshold = 0.5  # meter
+        # threshold = 0.1  # meter
         # num_nb = len(poses) - 1
         # # count number of neighbours
         # for j in range(len(poses)):
@@ -210,15 +211,36 @@ class StateMachine:
             tower_pose_dict = rospy.get_param('tower1_sim')
         else:
             tower_pose_dict = rospy.get_param('tower1')
+        # tower 1
+        # tower_pose = Pose()
+        # tower_pose.position.x = tower_pose_dict['x']
+        # tower_pose.position.y = tower_pose_dict['y']
+        # tower_pose.position.z = tower_pose_dict['z'] + self.cube_number*self.cube_dimension
+        # tower_pose.orientation.x = tower_pose_dict['qx']
+        # tower_pose.orientation.y = tower_pose_dict['qy']
+        # tower_pose.orientation.z = tower_pose_dict['qz']
+        # tower_pose.orientation.w = tower_pose_dict['qw']
+
+        # tower 2
         tower_pose = Pose()
         tower_pose.position.x = tower_pose_dict['x']
-        tower_pose.position.y = tower_pose_dict['y']
-        tower_pose.position.z = tower_pose_dict['z'] + self.cube_number*self.cube_dimension
         tower_pose.orientation.x = tower_pose_dict['qx']
         tower_pose.orientation.y = tower_pose_dict['qy']
         tower_pose.orientation.z = tower_pose_dict['qz']
         tower_pose.orientation.w = tower_pose_dict['qw']
 
+        if self.cube_number < 4:
+            tower_pose.position.y = tower_pose_dict['y'] + self.cube_number*self.cube_dimension
+            tower_pose.position.z = tower_pose_dict['z']
+        elif self.cube_number < 7:
+            tower_pose.position.y = tower_pose_dict['y']+ (self.cube_number-4+0.5)*self.cube_dimension
+            tower_pose.position.z = tower_pose_dict['z'] + self.cube_dimension
+        elif self.cube_number < 9:
+            tower_pose.position.y = tower_pose_dict['y'] + (self.cube_number-7+1)*self.cube_dimension
+            tower_pose.position.z = tower_pose_dict['z'] + 2*self.cube_dimension
+        elif self.cube_number == 9:
+            tower_pose.position.y = tower_pose_dict['y'] + 1.5*self.cube_dimension
+            tower_pose.position.z = tower_pose_dict['z'] + 3*self.cube_dimension
 
         return tower_pose
     
@@ -271,6 +293,16 @@ class StateMachine:
 
             pose_world = Pose()
             pose_world = pose_world_st.pose
+            # adjust tiny unrobost on orientation
+            orientation_list = [pose_world.orientation.x, pose_world.orientation.y, pose_world.orientation.z, pose_world.orientation.w]
+            (x_rot, y_rot, z_rot) = euler_from_quaternion(orientation_list)
+            if np.isclose(x_rot, 0, atol=1e-02):
+                x_rot = 0
+            if np.isclose(y_rot, 0, atol=1e-02):
+                y_rot = 0
+            if np.isclose(z_rot, 0, atol=1e-02):
+                z_rot = 0
+            (pose_world.orientation.x, pose_world.orientation.y, pose_world.orientation.z, pose_world.orientation.w) = quaternion_from_euler(x_rot, y_rot, z_rot)
             poses_world.append(pose_world)
         
         return poses_world
@@ -350,11 +382,12 @@ class StateMachine:
             print(">>>>> poses after transformation", transformed_poses)
             
             
-            target_pose = transformed_poses[0] #self.choose_target_pose(transformed_poses)
+            # target_pose = transformed_poses[0] #self.choose_target_pose(transformed_poses)
 
             # TODO: evaluate if poses are good, maybe filter before choosing the target?
             #       e.g. ignore poses that have negative z, or too high z coordinates
 
+            target_pose = self.choose_target_pose(transformed_poses)
             # Planning Scene
             self.planning_scene.set_cube_env(transformed_poses) 
 